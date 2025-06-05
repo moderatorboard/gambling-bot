9c68b25925d53a03cdce33007c4d80546d5c8842
moderatorboard <moderatorboard@outlook.com>
2025-06-05 13:42:46 +0700

    commit

---
+++ bot.ps1
@@ -0,0 +1,9 @@
+﻿Set-Location "C:\Users\Admin\Documents\GitHub\gambling-bot"
+
+"C:\Users\Admin\Documents\GitHub\gambling-bot\venv\Scripts\Activate.ps1"
+
+python.exe -m pip install --upgrade pip
+
+python.exe -m pip install discord.py python-dotenv aiosqlite PyNaCl
+
+python.exe -m main
